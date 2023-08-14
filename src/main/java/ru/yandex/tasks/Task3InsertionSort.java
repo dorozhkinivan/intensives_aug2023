c648bdd969865ee3322c65e4a0e6603e1c351eba
dorozhkinivan <dorozhkinivan123@gmail.com>
2023-08-14 19:11:41 +0300

    Update Task3InsertionSort.java
    done task3

--- src/main/java/ru/yandex/tasks/Task3InsertionSort.java
+++ src/main/java/ru/yandex/tasks/Task3InsertionSort.java
@@ -10,7 +10,18 @@ public class Task3InsertionSort {
          * Выход: отсортированный (сортировкой вставками!) numbers
          */
         // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return null;
+        int length = numbers.size();
+        for(int i = 1; i < length; i++){
+            int j = i;
+            while(j >= 1 && numbers.get(j-1) > numbers.get(j)){
+                int t = numbers.get(j-1);
+                numbers.set(j-1, numbers.get(j));
+                numbers.set(j, t);
+                j--;
+            }
+        }
+        return numbers;
+
     }
 
     public static void selfCheck() {
