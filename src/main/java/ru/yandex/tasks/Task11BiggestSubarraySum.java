a0778c68adab5e7ea056c7e9c15b115866672374
dorozhkinivan <dorozhkinivan123@gmail.com>
2023-08-14 18:42:34 +0300

    Update Task11BiggestSubarraySum.java
    done1

--- src/main/java/ru/yandex/tasks/Task11BiggestSubarraySum.java
+++ src/main/java/ru/yandex/tasks/Task11BiggestSubarraySum.java
@@ -2,14 +2,27 @@ package ru.yandex.tasks;
 
 public class Task11BiggestSubarraySum {
 
-    public static Subarray find(int[] numbers) {
+    public static ArrayList<Integer> sort(ArrayList<Integer> numbers) {
         /*
-         * Находит подмассив (подряд идущие элементы массива) с наибольшей суммой элементов
          * numbers: массив целых чисел, -10^5 <= numbers[i] <= 10^5, длина массива до 10^5
-         * Выход: Subarray
+         * Выход: отсортированный (сортировкой пузырьком!) numbers
          */
         // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return null;
+        int length = numbers.size();
+        for(int i = 0; i < length-1; i++){
+            boolean changed = false;
+            for(int j = i + 1; j < length; j++){
+                if (numbers.get(j) < numbers.get(i)){
+                    changed = true;
+                    int t = numbers.get(j);
+                    numbers.set(j, numbers.get(i));
+                    numbers.set(i, t);
+                }
+            }
+            if (!changed)
+                break;
+        }
+        return numbers;
     }
 
     public static void selfCheck() {
