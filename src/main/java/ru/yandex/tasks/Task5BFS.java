688ec1ecf0287630e57c9bdcd72bff140a09f3f1
dorozhkinivan <dorozhkinivan123@gmail.com>
2023-08-14 19:50:17 +0300

    Update Task5BFS.java
    task5 done

--- src/main/java/ru/yandex/tasks/Task5BFS.java
+++ src/main/java/ru/yandex/tasks/Task5BFS.java
@@ -3,11 +3,28 @@ package ru.yandex.tasks;
 import java.util.Arrays;
 
 public class Task5BFS {
-    public void runSearch() {
+    
+    static Queue<Integer> toCompute = new LinkedList<>();
+
+    static List<Integer> answer = new LinkedList<>();
+
+    public static void runSearch(int[][] tree, int root) {
+        toCompute.add(root);
         /*
          * Реализация bfs
          */
         // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
+        while(!toCompute.isEmpty()){
+            root = toCompute.poll();
+            if (tree[root][0] != -1) {
+                toCompute.add(tree[root][0]);
+            }
+            if (tree[root][1] != -1) {
+                toCompute.add(tree[root][1]);
+            }
+            answer.add(root);
+
+        }
     }
 
     public static int[] getBFSOrder(int[][] tree, int root) {
@@ -18,7 +35,14 @@ public class Task5BFS {
          * root - корень, откуда нужно начинать обход
          */
         // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return null;
+
+        toCompute.clear();
+        answer.clear();
+        runSearch(tree, root);
+        int[] ans = new int[answer.size()];
+        for(int i = 0; i < answer.size(); i++)
+            ans[i] = answer.get(i);
+        return ans;
     }
 
     public static void selfCheck() {
