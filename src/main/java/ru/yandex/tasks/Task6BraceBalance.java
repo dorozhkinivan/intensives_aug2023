318d311180e7ca8c3b170afccfa05cd66650a230
dorozhkinivan <dorozhkinivan123@gmail.com>
2023-08-14 20:06:28 +0300

    Update Task6BraceBalance.java
    task6 done

--- src/main/java/ru/yandex/tasks/Task6BraceBalance.java
+++ src/main/java/ru/yandex/tasks/Task6BraceBalance.java
@@ -7,7 +7,22 @@ public class Task6BraceBalance {
          * Выход: true/false, является ли строка ПСП
          */
         // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return true;
+        char[] charSequence = sequence.toCharArray();
+        Stack<Character> open = new Stack<>();
+        for(char c : charSequence){
+            if (c == '{' || c == '(' || c == '['){
+                open.push(c);
+            }
+            else {
+                if (open.isEmpty())
+                    return false;
+                char opened = open.pop();
+                if (!((c == '}' && opened == '{') || (c == ')' && opened == '(') || (c == ']' && opened == '['))){
+                    return false;
+                }
+            }
+        }
+        return open.isEmpty();
     }
 
     public static void selfCheck() {
